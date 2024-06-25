97953d41cd6a4cc822cd4ea54d4a2135af06924c
Aman <am123aman@gmail.com>
2024-06-25 09:16:43 +0530

    gameOver

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
+let count = 10;
+let gameOver = false;
+document.getElementById('cnt').innerHTML = count;
+
+let prguess = [];
+
+
+
 document.getElementById('form').addEventListener('submit', function(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const Value1 = document.getElementById('guessField');
-    const v1 = Value1.value;
+  if (gameOver) {
+    return; // Exit early if game is over
+  }
 
-    const feedback = document.getElementById('feedback');
+  const Value1 = document.getElementById('guessField');
+  const v1 = Value1.value;
 
-    if (v1 < 1 || v1 > 100) {
-        feedback.textContent = "Please input values between 1 and 100";
-    
-    } else {
-        feedback.textContent = "Your guess is " + v1;
-        // You can add further logic here for the game
-    }
+  const feedback = document.getElementById('feedback');
+
+  let flag = 0;
+
+  if (v1 < 1 || v1 > 100) {
+    feedback.textContent = "Please input values between 1 and 100";
+    feedback.style.border = '1px solid red';
+  } else {
+    feedback.textContent = "Your guess is " + v1;
+    flag = 1;
+    count = count - 1;
+    feedback.style.border = '1px solid green';
+    prguess.push(v1);
+
+    // You can add further logic here for the game
+  }
+
+  document.getElementById('cnt').innerHTML = count;
+
+  // Check if count has reached 0
+  if (count === 0) {
+    gameOver = true;
+    Value1.disabled = true; // Disable input field
+    feedback.textContent = "Game Over! You can no longer make guesses.";
+  }
+
+   let arr = document.getElementById('prg');
+   arr.innerText = prguess;
+
+  // Reset input field for the next guess
+  Value1.value = '';
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
       fontFamily: {
         sans: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'], // Example for Google Font
         custom: ['MyCustomFont', 'sans-serif'], // Example for Local Font
+        display: ['Playwrite_ES', 'sans-serif'],
+        playwright: ['Playwright', 'serif'],
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 2s ease-in-out',
       },
     },
   },
